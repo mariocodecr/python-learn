73e7f007c5ff8d5b600ee7e0315cca9b29f60d78
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-04-03 01:22:42 -0600

    Add hello world script with greeting function

---
+++ 01-helloworld/main.py
@@ -0,0 +1,9 @@
+
+def sayHello(name):
+    print("This script is running")
+    print(f'Hello, {name}!')
+
+
+if __name__ == '__main__':
+    print(f'Hi, World')  # Call the function with the argument 'Mario'')
+    sayHello('Mario')
