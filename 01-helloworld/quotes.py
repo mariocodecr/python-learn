024f3e4d0e3376d9896bf32d1707e8047ce5adba
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-04-05 23:03:15 -0600

    feat: add quotes script demonstrating string comparison and multi-line strings

---
+++ 01-helloworld/quotes.py
@@ -0,0 +1,26 @@
+
+
+text = 'Helo, python'
+text2 = "Hello, Python"
+
+result = text == text2
+print(result)
+
+message = 'Hello, Mario'
+print(message)
+
+#message = 'The code is from the user\'s'
+message = "The code is from the user's"
+message = "I'm developer"
+
+print(message)
+
+
+text_multi_line = """
+
+This is a multi-line string
+This is the second line
+
+"""
+
+print(text_multi_line)
