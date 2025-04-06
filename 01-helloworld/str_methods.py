c05444f264dbff2a21da851fa779e03a29cf8c42
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-04-05 23:37:58 -0600

    feat: enhance string methods demonstration with additional examples

--- 01-helloworld/str_methods.py
+++ 01-helloworld/str_methods.py
@@ -30,3 +30,32 @@ print(data_list[4])
 data = ['Mario2, Araya2, Python2, Next2, Nest2']
 text = ' '.join(data)
 print(text)
+
+text = 'Hello Mario, how are you?'
+print(text.find('Mario'))
+print(text.index('how'))
+
+print(text.startswith('Hello'))
+print(text.endswith('you?'))
+
+number = '1234'
+decimal = '1234.45'
+text = 'Python'
+mix = 'Python3'
+
+print(number.isnumeric())
+print(decimal.isdecimal())
+print(mix.isalnum())
+print(text.isalpha())
+
+text = '   hello Mario how are you, welcome to the python course   '
+text_clean = text.strip().capitalize().title()
+print(text_clean)
+
+
+new_text = text_clean.replace('Python Course', 'Python3 Course')
+print(new_text)
+
+
+words = new_text.split()
+print(words)
