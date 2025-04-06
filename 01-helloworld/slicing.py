374de466a0053608acba5ef757b23c4a90094827
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-04-06 00:05:46 -0600

    feat: add slicing demonstration script with various string manipulations

---
+++ 01-helloworld/slicing.py
@@ -0,0 +1,38 @@
+
+
+text = 'Mario Araya'
+
+print(text[0:6])
+print(text[7:])
+print(text[:6])
+print(text[::])
+print(text[::-1])
+
+text = 'Python'
+print(text[0:4])
+print(text[:4])
+print(text[2:])
+print(text[::3])
+
+
+text = "Hello world"
+new_text = text[:5] + text[5:].replace('world', 'Python')
+print(new_text)
+
+text = 'Python is great'
+parts = text.split(' ')
+parts2 = parts[:2]
+print(parts2)
+print(parts[2:])
+parts_reverse = parts[::-1]
+print(parts_reverse)
+text_reverse = ' '.join(parts_reverse)
+print(text_reverse)
+
+text = 'Python'
+print(text[:2].lower() + text[2:].upper())
+
+
+text = '   Hello Python   '
+print(text.strip()[:5])
+print(text.strip()[5:])
